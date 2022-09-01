cf0305672ee516a1b9d48c97e975d07c1af2967f
renanpmiranda <renanpmiranda5@gmail.com>
2022-09-01 14:01:48 -0300

    a010_exercicio02

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,11 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const arrayNumeros = [7, 12, 94, 28, 33, 40]
+const arrayString = ['faixa', 'preta', 'kimono', 'dourado']
+const arrayMix = [55, 21, 'lista', 'elemento', true, false]
+
+console.log(arrayNumeros.length)
+console.log(arrayString.length)
+console.log(arrayMix.length)
+
+console.log(arrayNumeros[0], arrayString[1], arrayMix[2])
+
+console.log(arrayNumeros.includes(7), arrayMix.includes(7))
